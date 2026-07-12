e99f48cfe86948ff1df89c53ec3a3d617a1d0d5d
Farhan <unrulywizkid001@gmail.com>
2026-07-12 22:27:20 +0000

    Fix webpack bundling libsql README files

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,14 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
+  serverExternalPackages: [
+    "@libsql/client",
+    "@libsql/isomorphic-fetch",
+    "@libsql/isomorphic-ws",
+    "@libsql/hrana-client",
+    "@prisma/adapter-libsql",
+    "cloudinary",
+  ],
 };
 
 export default nextConfig;
